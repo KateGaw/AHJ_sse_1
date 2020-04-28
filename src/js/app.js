4cc94b3510196b950b8828af1e8eadfa18e28fd9
kate_gaw <ekaterinagaw@gmail.com>
2020-04-28 14:27:23 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,37 @@
 import moment from 'moment';
 
 class Widget {
-  constructor(url) {
-    this.url = url;
-  }
-
-  init() {
-    this.widget = document.querySelector('#widget');
-    this.widgetList = document.querySelector('.widget-list');
-    this.events();
-  }
-
-  addMessage(message) {
-    const {
-      type,
-      mess,
-      date,
-      id,
-    } = JSON.parse(message);
-    this.itemMesage = document.createElement('li');
-    this.itemMesage.className = 'li-msg';
-    this.itemMesage.dataset.id = id;
-
-    let imgContainer;
-    if (type === 'freekick') {
-      imgContainer = '<img src=\'warn.png\'></img>';
-    } else if (type === 'goal') {
-      imgContainer = '<img src=\'goal.png\'></img>';
-    } else {
-      imgContainer = '<img class=\'img\'></img>';
+    constructor(url) {
+        this.url = url;
     }
 
-    this.itemMesage.innerHTML = `
+    init() {
+        this.widget = document.querySelector('#widget');
+        this.widgetList = document.querySelector('.widget-list');
+        this.events();
+    }
+
+    addMessage(message) {
+        const {
+            type,
+            mess,
+            date,
+            id,
+        } = JSON.parse(message);
+        this.itemMesage = document.createElement('li');
+        this.itemMesage.className = 'li-msg';
+        this.itemMesage.dataset.id = id;
+
+        let imgContainer;
+        if (type === 'freekick') {
+            imgContainer = '<img src=\'warn.png\'></img>';
+        } else if (type === 'goal') {
+            imgContainer = '<img src=\'goal.png\'></img>';
+        } else {
+            imgContainer = '<img class=\'img\'></img>';
+        }
+
+        this.itemMesage.innerHTML = `
           ${imgContainer}
           <div">
             <span>${moment(date).format('DD.MM.YY - HH:mm:ss')}</span>
@@ -39,25 +39,25 @@ class Widget {
            </div>
         `;
 
-    this.widgetList.appendChild(this.itemMesage);
-    this.widget.scrollTop = 9999;
-  }
+        this.widgetList.appendChild(this.itemMesage);
+        this.widget.scrollTop = 9999;
+    }
 
-  events() {
-    const eventSource = new EventSource(this.url);
+    events() {
+        const eventSource = new EventSource(this.url);
 
-    eventSource.addEventListener('open', () => {
-      console.log('connected');
-    });
+        eventSource.addEventListener('open', () => {
+            console.log('connected');
+        });
 
-    eventSource.addEventListener('error', (event) => {
-      console.log(event);
-    });
+        eventSource.addEventListener('error', (event) => {
+            console.log(event);
+        });
 
-    eventSource.addEventListener('comment', (event) => {
-      this.addMessage(event.data);
-    });
-  }
+        eventSource.addEventListener('comment', (event) => {
+            this.addMessage(event.data);
+        });
+    }
 }
 
-new Widget('http://localhost:7070/match').init();
+new Widget('https://sse-1.herokuapp.com/match').init();
